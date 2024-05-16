b57234fd0e90487d29054d0893c700ecc52e34f9
holerrrr <stastulenev5@gmail.com>
2024-05-16 21:07:15 +0300

    login + reg w/ new design

--- app/src/main/java/com/example/moneyminder/MainActivity.kt
+++ app/src/main/java/com/example/moneyminder/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.moneyminder
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
