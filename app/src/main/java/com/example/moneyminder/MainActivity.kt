e11dbafce0c7e5d942cb638e2893bc1b7248b280
holerrrr <stastulenev5@gmail.com>
2024-05-16 21:07:15 +0300

    login + reg w/ new design

--- app/src/main/java/com/example/moneyminder/MainActivity.kt
+++ app/src/main/java/com/example/moneyminder/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.moneyminder
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +17,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val loginButton = findViewById<Button>(R.id.button_login)
+        val regButton = findViewById<Button>(R.id.button_reg)
+
+        loginButton.setOnClickListener {
+            loginButton.setBackgroundResource(R.drawable.background_button_pressed)
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        regButton.setOnClickListener {
+            // Створюємо інтент для переходу на активність activity_login
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
