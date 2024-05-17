0f047c5b96665bda6b7053d8f5cf6297a9b49f76
holerrrr <stastulenev5@gmail.com>
2024-05-17 23:41:05 +0300

    w/ design some buttons 18.05.2004

--- app/src/main/java/com/example/moneyminder/LoginActivity.kt
+++ app/src/main/java/com/example/moneyminder/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.example.moneyminder
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,12 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val textViewRegister = findViewById<TextView>(R.id.textViewRegister)
+
+        textViewRegister.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
         }
     }
 }
